16e2ee1ed221de9c1c6640f2ccbb04899171c9eb
jereluo <jere.luomanmaki@outlook.com>
2025-07-01 17:50:34 +0300

    seo

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ function Footer() {
   <img
     src="assets/images/Sponsorit/koneharju.webp"
     style={{ height: '50px' }}
-    alt="Koneharju Oy"
+    alt="Koneharju Oy, Jylhä Racing tukija"
     className="sponsor-logo"
   />
 </a>
@@ -14,7 +14,7 @@ function Footer() {
 <a href="https://play.google.com/store/apps/details?id=com.jususteri.pathto100&hl=fi_FI" target="_blank" rel="noopener noreferrer">
   <img
     src="assets/images/Sponsorit/Path to 100.webp"
-    alt="Path to 100"
+    alt="Path to 100, Jylhä Racing tukija"
     style={{ height: '65px' }}
     className="sponsor-logo"
   />
@@ -23,7 +23,7 @@ function Footer() {
 <a href="https://www.autokorjaamohytonen.fi/" target="_blank" rel="noopener noreferrer">
   <img
     src="assets/images/Sponsorit/hytonen.webp"
-    alt="Autokorjaamo Velj. Hytönen Oy"
+    alt="Autokorjaamo Velj. Hytönen Oy, Jylhä Racing tukija"
     style={{ height: '50px' }}
     className="sponsor-logo"
   />
